a5595b3b31fa73bfee2364cf54c88d4dd0ff732b
CJBgon <c.j.bouwens@gmail.com>
2021-09-11 20:07:38 +0100

    loading in data

--- classify.py
+++ classify.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
+from sklearn import feature_extraction, linear_model, model_selection, preprocessing
+
+train_df = pd.read_csv('~/Documents/Side_Projects/binary_text_classification/nlp-getting-started/train.csv')
+test_df = pd.read_csv('~/Documents/Side_Projects/binary_text_classification/nlp-getting-started/test.csv')
+
+train_df[train_df["target"] == 0]["text"].values[1]
+train_df[train_df["target"] == 1]["text"].values[1]
+
+
+
 
