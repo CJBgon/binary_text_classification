a1b73d8a5d113253feb078f7eb7c43dd59abea30
CJBgon <c.j.bouwens@gmail.com>
2021-09-12 22:58:14 +0100

    running example

--- classify.py
+++ classify.py
@@ -10,4 +10,16 @@ train_df[train_df["target"] == 1]["text"].values[1]
 
 
 
+# feature extraction:
 
+# number of words or common words in distaster tweets
+
+count_vec = feature_extraction.text.CountVectorizer()
+
+
+count_train_vec = count_vec.fit_transform(train_df["text"])
+count_test_vec = count_vec.transform(test_df['text'])
+clf = linear_model.RidgeClassifier()
+
+scores = model_selection.cross_val_score(clf, count_train_vec, train_df["target"], cv=3, scoring="f1")
+scores
