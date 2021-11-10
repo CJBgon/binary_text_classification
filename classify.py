0bfa2c3c36213e135f757882e648ed78e9720952
CJBgon <48950814+CJBgon@users.noreply.github.com>
2021-11-10 14:54:06 +0000

    added crude XGBoost model

--- classify.py
+++ classify.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import xgboost as xgb
 from sklearn import feature_extraction, linear_model, model_selection, preprocessing, decomposition
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 
 train_df = pd.read_csv('/Users/christianbouwens/Documents/sideprojects/NLP_kaggle/train.csv')
 test_df = pd.read_csv('/Users/christianbouwens/Documents/sideprojects/NLP_kaggle/test.csv')
@@ -70,8 +71,72 @@ scores = model_selection.cross_val_score(lgrg,
     cv=3,
     scoring="f1")
 
-
-# XGBoost it?
+hashmat = feature_extraction.text.HashingVectorizer()
+hashmat_train = hashmat.fit_transform(train_df["text"])
+scores = model_selection.cross_val_score(clf,
+    hashmat_train,
+    train_df["target"],
+    cv=3,
+    scoring="f1")
+# hashmat already gives a decent approximation.
+# what if we provide all features to an XGBclassifier
 import xgboost as xgb
+
+# features:
+
+hashmat_train
+count_train_vec
+freq_train_vec
+
+
+X = scipy.sparse.hstack([count_train_vec, freq_train_vec, hashmat_train])
+y = train_df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+# set up quick random XGBClassifier
+xgbclass = xgb.XGBClassifier()
+
+params = {
+    'objective':['binary:logistic'],
+    'learning_rate':[0.01, 0.05, 0.1, 0.15, 0.5],
+    'colsample_bytree':[0.01, 0.1, 0.3, 0.6],
+    'max_depth':[3, 4, 7, 10, 15, 20],
+    'alpha':[0.01,0.1, 1, 3, 5, 7, 10, 15],
+    'lambda':[0.01,0.1,0.5,1],
+    'gamma':[0.01, 0.1, 0.5, 1, 2, 4, 5,],
+    'subsample':[0.5,0.7,1]
+}
+
+random_search = RandomizedSearchCV(
+    xgbclass,
+    param_distributions = params,
+    cv = 3,
+    scoring = "f1")
+
+random_search.fit(X_train, y_train)
+
+random_search.best_score_
+random_search.score(X_test, y_test)
+
+y_train_hat = random_search.predict(X_train)
+y_test_hat = random_search.predict(X_test)
+
+print('train performance')
+print('--------------------------------')
+print(classification_report(y_train, y_train_hat))
+
+print('test performance')
+print('--------------------------------')
+print(classification_report(y_test, y_test_hat))
+
+print('ROC_AUC score')
+print('--------------------------------')
+print(roc_auc_score(y_test, y_test_hat))
+print('')
+
+print('Confusion Matrix')
+print('--------------------------------')
+print(confusion_matrix(y_test, y_test_hat))
 # this score is on the training set, how does it perform on the testing set?
 
